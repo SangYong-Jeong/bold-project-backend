0f22b411fcbc86132e10850f46d0dc3d2de4d0a7
SangYong-Jeong <mmaf@naver.com>
2021-12-06 20:45:20 +0900

    Character Model CRUD 구현

--- src/api/design/character/character.ctrl.js
+++ src/api/design/character/character.ctrl.js
@@ -1,6 +1,6 @@
 const Character = require('../../../models/design/character');
 
-// POST /api/design/character
+// POST /api/design/character - CREATE
 exports.write = async (ctx) => {
   const { number, title, content, imgs, publishedDate } = ctx.request.body;
   const character = new Character({
@@ -18,14 +18,55 @@ exports.write = async (ctx) => {
   }
 };
 
-// GET /api/design/character
-exports.list = (ctx) => {};
+// GET /api/design/character - READ
+exports.list = async (ctx) => {
+  try {
+    const characters = await Character.find().exec();
+    ctx.body = characters;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+};
 
-// GET /api/design/character/:id
-exports.read = (ctx) => {};
+// GET /api/design/character/:id - READ
+exports.read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const character = await Character.findById(id).exec();
+    if (!character) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = character;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+};
 
-// DELETE /api/design/character/:id
-exports.remove = (ctx) => {};
+// DELETE /api/design/character/:id - REMOVE
+exports.remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Character.findByIdAndDelete(id).exec();
+    ctx.status = 204;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+};
 
-// PATCH /api/design/character/:id
-exports.update = (ctx) => {};
+// PATCH /api/design/character/:id - UPDATE
+exports.update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const character = await Character.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // new option이 true이면 업데이트 된 데이터 반환
+    }).exec();
+    if (!character) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = character;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+};
