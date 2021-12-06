26caae4bcb9eb6cdca3149f146a4fc4e891c210c
SangYong-Jeong <mmaf@naver.com>
2021-12-06 22:52:13 +0900

    character collection에 dummy data 추가

--- src/modules/mongo-init.js
+++ src/modules/mongo-init.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+// const dummyCreator = require('../dummyData');
 
 module.exports = async function main(MONGO_URI) {
   try {
     await mongoose.connect(MONGO_URI);
     console.log('Connected to MongoDB');
+    // dummyCreator();
   } catch (err) {
     console.log(err);
   }
